9067dbefa5a765c63851935d04ddf5f3e9620cf7
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2025-06-12 16:41:21 +0530

    modal import change

--- src/views/modals/speakers/index.jsx
+++ src/views/modals/speakers/index.jsx
@@ -8,18 +8,18 @@ import Speaker7Modal from "./speaker7modal";
 import Speaker8Modal from "./speaker8modal";
 import Speaker9Modal from "./speaker9modal";
 import Speaker10Modal from "./speaker10modal";
-import Speaker11Modal from "./speaker11modal";
+import Speaker11Modal from "./speaker11modal"; 
 import Speaker12Modal from "./speaker12modal";
 import Speaker13Modal from "./speaker13modal";
-import Speaker14Modal from "./speaker14Modal";
-import Speaker15Modal from "./speaker15Modal";
-import Speaker16Modal from "./speaker16Modal";
-import Speaker17Modal from "./speaker17Modal";
-import Speaker18Modal from "./speaker18Modal";
-import Speaker19Modal from "./speaker19Modal";
-import Speaker20Modal from "./speaker20Modal";
-import Speaker21Modal from "./speaker21Modal";
-import Speaker22Modal from "./speaker22Modal";
+import Speaker14Modal from "./speaker14modal";
+import Speaker15Modal from "./speaker15modal";
+import Speaker16Modal from "./speaker16modal";
+import Speaker17Modal from "./speaker17modal";
+import Speaker18Modal from "./speaker18modal";
+import Speaker19Modal from "./speaker19modal";
+import Speaker20Modal from "./speaker20modal";
+import Speaker21Modal from "./speaker21modal";
+import Speaker22Modal from "./speaker22modal";
 export {
     Speaker1Modal,
     Speaker2Modal,
